0e9061081b685b244e999a406979560676cccff4
sltn011 <61147580+sltn011@users.noreply.github.com>
2022-04-29 19:49:30 +0300

    Improved first test

--- Source/UE5_Tests/Private/Tests/SandboxTests.cpp
+++ Source/UE5_Tests/Private/Tests/SandboxTests.cpp
@@ -11,5 +11,15 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(FMathMaxInt, TEXT("UE5_Tests.Math.MaxInt"),
 
 bool FMathMaxInt::RunTest(FString const &Parameters)
 {
-    return TestTrue("Two Different Positive int32", FMath::Max(10, 100) == 100);
+    TestTrue("Two Different Positive int32", FMath::Max(10, 100) == 100);
+    TestTrue("Two Equal Positive int32", FMath::Max(10, 10) == 10);
+
+    TestTrue("Two Different Negative int32", FMath::Max(-10, -100) == -10);
+    TestTrue("Two Equal Negative int32", FMath::Max(-10, -10) == -10);
+
+    TestTrue("Zero and Positive int32", FMath::Max(0, 100) == 100);
+    TestTrue("Two Zeroes int32", FMath::Max(0, 0) == 0);
+    TestTrue("Zero and Negative int32", FMath::Max(-100, 0) == 0);
+
+    return true;
 }

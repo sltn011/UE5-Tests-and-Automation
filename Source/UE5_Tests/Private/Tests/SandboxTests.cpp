df1ec257620cb6792675af394545a3b4588e2cba
sltn011 <61147580+sltn011@users.noreply.github.com>
2022-04-29 20:35:50 +0300

    Second test added

--- Source/UE5_Tests/Private/Tests/SandboxTests.cpp
+++ Source/UE5_Tests/Private/Tests/SandboxTests.cpp
@@ -6,10 +6,10 @@
 
 #include "Misc/AutomationTest.h"
 
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FMathMaxInt, TEXT("UE5_Tests.Math.MaxInt"),
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FMathMaxIntTest, TEXT("UE5_Tests.Math.MaxInt"),
     EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
 
-bool FMathMaxInt::RunTest(FString const &Parameters)
+bool FMathMaxIntTest::RunTest(FString const &Parameters)
 {
     AddInfo("FMath::Max<int32> Testing");
 
@@ -27,6 +27,23 @@ bool FMathMaxInt::RunTest(FString const &Parameters)
     TestTrueExpr(FMath::Max(0, 0) == 0);
     TestTrueExpr(FMath::Max(-100, 0) == 0);
 
+
+    return true;
+}
+
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FMathSqrtTest, TEXT("UE5_Tests.Math.Sqrt"),
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
+
+bool FMathSqrtTest::RunTest(const FString &Parameters)
+{
+    AddInfo("FMath::Sqrt Testing");
+
+    TestEqual("FMath::Sqrt(4) [0]", FMath::Sqrt(4.0f), 2.0f);
+    TestEqual("FMath::Sqrt(4) [1]", FMath::Sqrt(3.0f), 1.7f, 0.1f);
+    TestEqual("FMath::Sqrt(3) [2]", FMath::Sqrt(3.0f), 1.73f, 0.01f);
+    TestEqual("FMath::Sqrt(0) [3]", FMath::Sqrt(3.0f), 1.73205f);
 
     return true;
 }
